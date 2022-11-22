cf12a8870b4184543ab297a37392bf66148708b9
Tahar Kessi <tahar.kessi@gmail.com>
2022-11-22 21:12:53 +0100

    tout corrigé l'ekip

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,12 @@
 class Car < ApplicationRecord
+  BRANDS = ["Abarth", "Alfa Roméo", "Alpine", "Austin", "Aston Martin", "Audi", "BMW", "Citroen", "Honda", "Jaguar", "Lada",
+            "Lancia", "Mazda", "Mercedes", "Nissan", "Opel", "Peugeot", "Porsche", "Renault", "Seat", "Subaru", "Toyota",
+            "Volkswagen", "Volvo"]
 
-  BRANDS = ['Abarth','Alfa Roméo','Alpine','Austin', 'Aston Martin','Audi', 'BMW', 'Citroen', 'Honda', 'Jaguar', 'Lada', 'Lancia', 'Mazda', 'Mercedes', 'Nissan', 'Opel', 'Peugeot', 'Porsche', 'Renault', 'Seat', 'Subaru', 'Toyota', 'Volkswagen', 'Volvo']
-  
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
+  # has_one_attached :photo
 
   validates :brand, :year, :modele, :price, :description, presence: true
   validates :brand, inclusion: { in: BRANDS }
-
 end
